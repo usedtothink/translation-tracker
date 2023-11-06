8de807e23ea9a8d76811c662b504ff2385ce4e43
margaret <usedtothink@gmail.com>
2023-11-06 12:07:56 -0600

    Initial build of CreateTranlsationCaseRequest, started CreateTranslationCaseActivity

---
+++ TranslationTrackerLambda/src/main/java/translationtracker/CreateTranslationCaseActivity.java
@@ -0,0 +1,35 @@
+package translationtracker;
+
+
+import javax.inject.Inject;
+
+/**
+ * Implementation of the CreateTranslationCaseActivity for the TranslationTracker's CreateTranslationCase API.
+ * <p>
+ * This API allows the customer to create a new translation case that is linked to a translation client.
+ */
+
+public class CreateTranslationCaseActivity {
+    private final TranslationCaseDao caseDao;
+
+    /**
+     * Instantiates a new CreateTranslationCaseActivity object.
+     *
+     * @param translationCaseDao to access the translation cases table.
+     */
+    @Inject
+    public CreateTranslationCaseActivity(TranslationCaseDao caseDao) {
+        this.caseDao = caseDao;
+        /**
+         * This method handles the incoming request by persisting a new translation case
+         * with the provided case nickname and translation client ID from the request.
+         * <p>
+         * Then it returns the newly created translation case.
+         * <p>
+         * If the provided case nickname already exists, throw a
+         * Duplicate
+         */
+    }
+
+
+}
